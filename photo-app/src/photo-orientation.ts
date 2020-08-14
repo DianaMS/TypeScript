90983b20f27f77e6114fb67d76dd6b948ec1b638
DianaMS <dmartinezs@uni.pe>
2020-08-13 20:43:16 -0500

    exportación de clases

---
+++ photo-app/src/photo-orientation.ts
@@ -0,0 +1,6 @@
+export enum PhotoOrientation {
+    Portrait,
+    Square,
+    Panorama,
+    Landscape 
+}
---
+++ photo-app/src/picture.ts
@@ -0,0 +1,18 @@
+import { Item } from './item';
+import { PhotoOrientation } from './photo-orientation';
+
+export class Picture extends Item{
+   
+    constructor(id: number,
+                title: string,
+                private date: string,
+                private orientation: PhotoOrientation) {
+        super(id, title);
+    }
+
+    toString() {
+        return `[id: ${this.id},
+                title: ${this.title},
+                orientation: ${this.orientation}]`;    
+    }
+}
